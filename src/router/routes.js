bcae38cc6a7d962f11aa392605c7074b4ecd8a67
AlexMartin <alex.martin.7e7@itb.cat>
2025-03-18 23:19:01 +0100

    commit primeros avances

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,9 +3,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Home.vue') },
-      { path: 'items', component: () => import('pages/ItemsList.vue') },
-      { path: 'item/:id', component: () => import('pages/ItemDetail.vue') }
+      { path: '', component: () => import('../pages/Home.vue') },
+      { path: 'items', component: () => import('../components/ItemList.vue') },
+      { path: 'item/:id', component: () => import('../components/ItemDetail.vue') }
     ]
   }
 ];
