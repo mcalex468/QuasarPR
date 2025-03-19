37cc157d129ff12cdeb8f29fc1a3c8ab52f04084
mcalex468 <alexmaco468@gmail.com>
2025-03-19 19:06:24 +0100

    comit quasar

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,11 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('../pages/Home.vue') },
+      { path: '', component: () => import('../pages/HomePage.vue') },
       { path: 'items', component: () => import('../components/ItemList.vue') },
-      { path: 'item/:id', component: () => import('../components/ItemDetail.vue') }
-    ]
-  }
-];
+      { path: 'item/:id', component: () => import('../components/ItemDetail.vue') },
+    ],
+  },
+]
 
-export default routes;
+export default routes
